922a34990c1e99c89a210dee12f8c998f1538998
Rifat Fazlutdinov <46864568+Rifat-Fazlutdinov@users.noreply.github.com>
2020-09-10 18:51:23 +0500

    removes extra comments

--- app/views.py
+++ app/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
-from .serializers import URLSerializer
-from .models import URL
+
 from rest_framework import viewsets
-from rest_framework.response import Response
 from rest_framework.mixins import CreateModelMixin
 
-# Create your views here.
+from .serializers import URLSerializer
+from .models import URL
+
+
 class URLViewSet(viewsets.GenericViewSet, CreateModelMixin):
     serializer_class = URLSerializer
     def retrieve(self, request, slug):
